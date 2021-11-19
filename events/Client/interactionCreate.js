03a278244d0a9b0539d531ce71b0cf03a24acdd5
RiZeFlame <oliver.flynn025@gmail.com>
2021-11-19 15:13:48 -0800

    defer all interactions 😐

--- events/Client/interactionCreate.js
+++ events/Client/interactionCreate.js
@@ -4,13 +4,16 @@ const errors = require("../../utilities/errors.json");
 const cooldowns = new Discord.Collection();
 
 module.exports = async (client, interaction) => {
-
 	if(!interaction.guild.me.permissions.has("SEND_MESSAGES") || !interaction.guild.me.permissionsIn(interaction.channel).has("SEND_MESSAGES")) return client.logger.error(`Missing Message permission in ${interaction.guild.id}`);
 
 	//get support server and check if interaction user is a donator
 	const supportGuild = client.guilds.cache.get("740705740221841450");
 	const member = supportGuild.members.cache.get(interaction.user.id);
 	const isDonator = member ? member.roles.cache.some(role => role.id === "773021050438287390") : false;
+
+	interaction.deferReply({
+		ephemeral: false
+	});
 
 	//check if interaction is a command
 	if (interaction.isCommand()){
@@ -30,7 +33,7 @@ module.exports = async (client, interaction) => {
 		//check if command requires a certain permission and if user has permission
 		if (command.permission && !interaction.member.permissions.has(command.permission)) {
 			embed.setDescription(`You do not have sufficient permissions to use this command. \n **REQUIRED PERMISSIONS:** ${command.permission.join(" ")}`);
-			return interaction.reply({embeds: [embed]});
+			return interaction.editReply({embeds: [embed]});
 		}
 
 		//get music player
@@ -39,39 +42,39 @@ module.exports = async (client, interaction) => {
 		//check if command requires player and if it doesn't exist
 		if (command.player && !player) {
 			embed.setDescription("There is no music player for this guild.");
-			return interaction.reply({embeds: [embed]});
+			return interaction.editReply({embeds: [embed]});
 		}
 
 		//check if command requires user to be in voice channel and user is in a voice channel
 		if (command.inVoiceChannel && !interaction.member.voice.channel) {
 			embed.setDescription("You must be in a voice channel to use this!");
-			return interaction.reply({embeds: [embed]});
+			return interaction.editReply({embeds: [embed]});
 		}
 
 		//check if command requires user to be in the same voice channel as the bot and if user is in same voice channel
 		if (command.sameVoiceChannel && interaction.member.voice.channel !== interaction.guild.me.voice.channel) {
 			embed.setDescription(`You must be in the same channel as ${interaction.client.user} to use this!`);
-			return interaction.reply({embeds: [embed]});
+			return interaction.editReply({embeds: [embed]});
 		}
 
 		//check if command is marked as nsfw and if channel command was executed in is marked as nsfw
 		if (command.nsfw && !interaction.channel.nsfw){
-			interaction.reply({content: errors[(Math.floor(Math.random() * Math.floor(errors.length)))]});
+			interaction.editReply({content: errors[(Math.floor(Math.random() * Math.floor(errors.length)))]});
 		}
 
 		//check if command is guild only and if command was executed in guild
 		if (command.guildOnly && interaction.channel.type !== "GUILD_TEXT") {
-			return interaction.reply({content: "I can't execute that command inside DMs!"});
+			return interaction.editReply({content: "I can't execute that command inside DMs!"});
 		}
 		
 		//check if command is donor only and if user is a donator
 		if(command.Donor && !isDonator){
-			return interaction.reply({content: "You must donate to use that command"});
+			return interaction.editReply({content: "You must donate to use that command"});
 		}
 	
 		//check if command is owner only and if config includes the users id
 		if(command.owner && !client.config.ownerID.includes(interaction.user.id)){
-			return interaction.reply({content: "You must own the bot use that command"});
+			return interaction.editReply({content: "You must own the bot use that command"});
 		}
 
 		//check if cooldowns collection doesnt have command
@@ -91,7 +94,7 @@ module.exports = async (client, interaction) => {
 			//check how long the cooldown is and tell that to user
 			if (now < expirationTime) {
 				const timeLeft = (expirationTime - now) / 1000;
-				return interaction.reply({content: `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`, ephemeral: true});
+				return interaction.editReply({content: `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`, ephemeral: true});
 			}
 		}
 
@@ -103,7 +106,7 @@ module.exports = async (client, interaction) => {
 		    command.execute(client, interaction, args);
 	    } catch (error) {
 		    client.logger.error(error);
-		    interaction.reply(`There was an error executing that command\nError:${error} Please contact ${client.users.cache.get(client.config.ownerID[0]).tag} if this error continues`);
+		    interaction.editReply(`There was an error executing that command\nError:${error} Please contact ${client.users.cache.get(client.config.ownerID[0]).tag} if this error continues`);
 		    supportGuild.channels.cache.get("844390085448564746").send(`${error} \n Command executed: ${command.name}`);
 	    }
 	}
@@ -116,7 +119,7 @@ module.exports = async (client, interaction) => {
 		    button.execute(client, interaction);
 	    } catch (error) {
 		    client.logger.error(error);
-		    interaction.reply(client.lang("cmd-error", "en").replace("{ERROR", error).replace("{BOT OWNER}", client.users.cache.get(client.config.ownerID[0]).tag));
+		    interaction.editReply(client.lang("cmd-error", "en").replace("{ERROR}", error).replace("{BOT OWNER}", client.users.cache.get(client.config.ownerID[0]).tag));
 		    supportGuild.channels.cache.get("844390085448564746").send(`${error} \n button pressed: ${button.name}`)
 	    }
 	}
