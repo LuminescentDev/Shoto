504ff2bfdfb72ced48695af4357f42af930b5f90
Oli/AkiraDev <akira.devjs@gmail.com>
2021-11-21 10:18:44 -0800

    bruh

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -76,6 +76,10 @@ module.exports = async client => {
 	client.logger.info(`Registered ${commands.size} commands!`);
 	client.manager.init(client.user.id);
 	client.stats.autopost();
+	//register custom field for statcord
+	client.stats.registerCustomFieldHandler(1, async client => {
+		return client.manager.nodes.map(node => node.stats.players).reduce((a, b) => a + b, 0).toString();
+	});
 	await sleep(2000);
 	const timer = (Date.now() - start) / 1000;
 	client.logger.info(`Done (${timer}s)! I am running!`);
