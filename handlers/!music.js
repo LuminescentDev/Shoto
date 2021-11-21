354a13f8e3851c2012a7d61a5d5b654f87385563
Oli/AkiraDev <akira.devjs@gmail.com>
2021-11-21 10:24:02 -0800

    i hate my life

--- handlers/!music.js
+++ handlers/!music.js
@@ -2,7 +2,6 @@ const { Manager, Structure } = require("erela.js");
 const { LavasfyClient } = require("lavasfy");
 const apple = require("erela.js-apple");
 const deezer = require("erela.js-deezer");
-const fs = require("fs");
 
 // This system from discord music bot https://github.com/SudhanPlayz
 Structure.extend(
@@ -27,10 +26,10 @@ module.exports = client => {
 			autoResolve: true,
 			useSpotifyMetadata: true,
 		},
-		nodes,
+		client.config.nodes,
 	);
 	client.manager = new Manager({
-		nodes: nodes,
+		nodes: client.config.nodes,
 		send: (id, payload) => {
 			const guild = client.guilds.cache.get(id);
 			if (guild) guild.shard.send(payload);
@@ -42,14 +41,4 @@ module.exports = client => {
 		],
 	});
 	client.on("raw", d => client.manager.updateVoiceState(d));
-	fs.readdir("./events/music/", (err, files) => {
-		if (err) return client.logger.error(err);
-		files.forEach(file => {
-			if (!file.endsWith(".js")) return;
-			const event = require(`../events/music/${file}`);
-			const eventName = file.split(".")[0];
-			client.manager.on(eventName, event.bind(null, client));
-		});
-		// goes through all the files in the events folder and registers them
-	});
 };
