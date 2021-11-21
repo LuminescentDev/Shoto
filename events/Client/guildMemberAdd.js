f665443dc6f3fde39eda30a7573b5e56efb5042f
Oli/AkiraDev <akira.devjs@gmail.com>
2021-11-21 11:23:40 -0800

    do some stuff

--- events/Client/guildMemberAdd.js
+++ events/Client/guildMemberAdd.js
@@ -1,12 +1,13 @@
 function nth(n){return["st","nd","rd"][((n+90)%100-10)%10-1]||"th"}
 module.exports = async (client, member) => {
-	const settings = await client.getSettings(member.guild.id);
+	const settings = await client.getSettings(member);
 
 	//if join channel isnt defined or if leave message is set to false dont send message
 	if(!settings.joinMessage || settings.joinMessage.toLowerCase() === "false" || !settings.joinChannelID || !member.guild.channels.cache.get(settings.joinChannelID)) return;
 	const joinMessage = settings.joinMessage
         .replace("{USER MENTION}", `<@${member.user.id}>`)
         .replace("{USER TAG}", member.user.tag)
+		.replace("{USERNAME}", member.user.username)
         .replace("{SERVER NAME}", member.guild.name)
         .replace("{MEMBER COUNT}", nth(member.guild.members.cache.size));
 	return member.guild.channels.cache.get(settings.joinChannelID).send({content: joinMessage});
