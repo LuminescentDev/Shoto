44ab4cebef5da0f97bcf9e24d46e4071e3d50a4a
Oli/AkiraDev <akira.devjs@gmail.com>
2021-11-22 21:46:10 -0800

    fix

--- commands/admin/announce.js
+++ commands/admin/announce.js
@@ -20,6 +20,8 @@ module.exports = {
 		//get all settings
 		const settings = await client.getSettings(interaction);
 
+		console.log(settings);
+
 		//if no channel reply with error
 		if (!settings.AchannelID) {
 			return interaction.editReply({content: client.lang("no-config", settings.language), ephemeral: true});
@@ -33,7 +35,7 @@ module.exports = {
                     .setColor(client.embedColor)
                     .setDescription(`${args[0]}`)
                     .setFooter(`${interaction.member.user.username}`, `${interaction.member.user.displayAvatarURL({ dynamic: true, size: 2048 })}`);
-		interaction.guild.channels.cache.get(settings[0].AchannelID).send({embeds: [announcmentEmbed]});
+		interaction.guild.channels.cache.get(settings.AchannelID).send({embeds: [announcmentEmbed]});
 		interaction.editReply({content: "Message Sent!", ephemeral: false});
 
 	},
