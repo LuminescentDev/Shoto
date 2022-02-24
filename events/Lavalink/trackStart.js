95f64462925bc85e13fa8c6556134c2d7f881970
Akira <67001116+Oli-idk@users.noreply.github.com>
2022-02-24 11:26:27 -0800

    fix music?

--- events/Lavalink/trackStart.js
+++ events/Lavalink/trackStart.js
@@ -1,12 +1,12 @@
 const { MessageEmbed } = require("discord.js");
-
+const { convertTime } = require("../../utilities/utilities.js")
 module.exports = async (client, player, track, payload) => {
 
 	const channel = client.channels.cache.get(player.textChannel);
 	const emojiplay = client.emoji.play;
 
 	const embed = new MessageEmbed()
-        .setDescription(`${emojiplay} **Started Playing**\n [${track.title}](${track.uri}) - \`[${convertTime.convertTime(track.duration)}]\` [<@${track.requester.id}>]`)
+        .setDescription(`${emojiplay} **Started Playing**\n [${track.title}](${track.uri}) - \`[${convertTime(track.duration)}]\` [<@${track.requester.id}>]`)
         .setThumbnail(track.displayThumbnail("3"))
         .setColor(client.embedColor)
         .setTimestamp();
