ead4d3f0d1da71d24509469658afec119050574a
Akira <67001116+Oli-idk@users.noreply.github.com>
2022-04-26 09:05:02 -0700

    Fix ready.js?

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -3,7 +3,7 @@ const start = Date.now();
 module.exports = async client => {
 
 	client.logger.info(`I am running`);
-	client.logger.info(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers`);
+	client.logger.info(`Ready to serve in ${client.channels.size} channels on ${client.groups.size} servers`);
 	client.logger.info(`Registered ${client.commands.size} commands!`);
 	const timer = (Date.now() - start) / 1000;
 	client.logger.info(`Done (${timer}s)! I am running!`);
